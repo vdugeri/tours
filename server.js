f2f399e381457d5bfcf3c58fa3b0aae96f42dc1d
vdugeri <verem.dugeri@gmail.com>
2019-01-28 18:11:56 +0100

    refactor

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const app = express();
 app.use(express.static('src'));
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname + './src/index.html'));
+  res.sendFile(path.join(__dirname + '/index.html'));
 });
 
 
